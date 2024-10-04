8a0e1c5af1d8b9d1cf0977e390dd433df89292d0
Tu Nombre <you@example.com>
2024-10-04 10:11:02 -0700

    arregla las tablas

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ app.set('view engine', 'ejs');
 // Middleware para manejar datos de formularios
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Función para obtener el color basado en las coordenadas
-function getColor(m, n) {
-    const colors = ["red", "blue", "green", "yellow"];
-    const colorIndex = (m + n) % colors.length;
-    return colors[colorIndex];
+// Función para obtener el color basado en las coordenadas (gradiente)
+function getColor(m, n, maxM, maxN) {
+    const red = Math.floor((m / maxM) * 255);
+    const green = Math.floor((n / maxN) * 255);
+    const blue = Math.floor(((maxM - m) / maxM) * 255);
+    return `rgb(${red}, ${green}, ${blue})`;
 }
 
 // Ruta para mostrar el formulario

7bf1102ed14132f150d57c3646a255704648f875
Tu Nombre <you@example.com>
2024-10-04 10:05:55 -0700

    arregla las tablas

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3021;
+const port = 3022;
 
 // Configurar el motor de plantillas EJS
 app.set('view engine', 'ejs');
